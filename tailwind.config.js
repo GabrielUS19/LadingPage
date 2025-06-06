286579a25dff7cfb2cce2addfdb0167fcc4b9ba1
GabrielUS19 <gabrieluchoa2607@gmail.com>
2025-06-05 21:35:44 -0300

    Criei uma interface de navegação e a primeira seção

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inconsolata: ['Inconsolata', 'Times New Roman', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
