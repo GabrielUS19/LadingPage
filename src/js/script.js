fc057b4e15fbdfb3c42c7fdd8c91d2c74c614ed3
GabrielUS19 <gabrieluchoa2607@gmail.com>
2025-06-16 21:24:41 -0300

    Adicionei um JS para aplicar sombra no header ao rolar e para detectar em qual página estou

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,10 @@
 const toggleButton = document.getElementById("toggle-nav");
 const mobileMenu = document.getElementById("mobile-menu");
 const menuIcon = document.getElementById("menu-icon");
+const header = document.getElementById("header");
+
+const links = document.querySelectorAll('[data-class="nav-link"]');
+const sections = document.querySelectorAll(".section");
 
 toggleButton.addEventListener("click", function () {
   mobileMenu.classList.toggle("hidden");
@@ -10,4 +14,29 @@ toggleButton.addEventListener("click", function () {
   } else {
     menuIcon.src = "assets/menu-rows.png";
   }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY !== 0) {
+    header.classList.add("shadow-xl/20");
+  } else {
+    header.classList.remove("shadow-xl/20");
+  }
+
+  sections.forEach((section) => {
+    const beginPoint = section.offsetTop - 300;
+    const endPoint = beginPoint + section.clientHeight + 100;
+
+    if (window.scrollY >= beginPoint && window.scrollY <= endPoint) {
+      links.forEach((link) => {
+        var index = link.href;
+
+        if (index.substring(index.indexOf("#") + 1) == section.id) {
+          link.classList.add("active-link");
+        } else {
+          link.classList.remove("active-link");
+        }
+      });
+    }
+  });
 });
